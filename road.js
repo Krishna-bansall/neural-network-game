4722060bdc7fcae442346060d19aa1db42582c1a
krishna-bansall <bansalkrishna321go@gmail.com>
2022-08-10 23:23:09 +0530

    Road Fixed

--- road.js
+++ road.js
@@ -16,19 +16,13 @@ class Roads {
 		ctx.lineWidth = 5;
 		ctx.strokeStyle = "white";
 
-		for (let i = 0; i < array.length; i++) {
-			const element = array[i];
+		for (let i = 1; i < this.laneCount; i++) {
+			const x = lerp(this.left, this.right, i / this.laneCount);
+
+			ctx.beginPath();
+			ctx.moveTo(x, this.top);
+			ctx.lineTo(x, this.bottom);
+			ctx.stroke();
 		}
-
-		ctx.beginPath();
-		ctx.moveTo(this.right, this.top);
-		ctx.lineTo(this.right, this.bottom);
-		ctx.stroke();
-
-		ctx.beginPath();
-		ctx.moveTo(this.left, this.top);
-		ctx.lineTo(this.left, this.bottom);
-		ctx.stroke();
-		ctx.save();
 	};
 }
